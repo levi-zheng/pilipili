29c2221bbe497c0fa4998d0456fa1bb3ebf1b1cc
levizheng <levi.zheng@outlook.com>
2024-04-28 21:57:30 +0800

    feat(gateway): init gateway

---
+++ pilipili-gateway/src/main/java/com/levizheng/pilipili/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.levizheng.pilipili.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author levi
+ * @since 2024/4/13
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
