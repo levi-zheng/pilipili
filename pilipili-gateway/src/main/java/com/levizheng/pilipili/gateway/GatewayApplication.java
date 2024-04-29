af8301afbae181133fe0dc36f2b2ec5361f627b1
levizheng <levi.zheng@outlook.com>
2024-04-29 13:39:40 +0800

    feat(user): init user

--- pilipili-gateway/src/main/java/com/levizheng/pilipili/gateway/GatewayApplication.java
+++ pilipili-gateway/src/main/java/com/levizheng/pilipili/gateway/GatewayApplication.java
@@ -1,5 +1,6 @@
 package com.levizheng.pilipili.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,10 +9,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author levi
  * @since 2024/4/13
  */
+@Slf4j
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
+        log.info("网关服务启动中...");
         SpringApplication.run(GatewayApplication.class, args);
     }
 }
